a0bb0bef00fa805a2bc0153e88a1d4505c6d188c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-04 22:51:09 +0200

    CodeWars - excercises 3 day

--- src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExcercise.java
+++ src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExcercise.java
@@ -11,8 +11,16 @@ public class CodeWarsExcercise {
                     int invertn = array[i];
                     array[i] = invertn * (-1);
                 }
-                return null;
+                for(int i = 0; i < array.length; i++) {
+                    array[i] *= -1;
+                }
+                for (int i = 0;i < array.length; i++)
+                    array[i] = -(array[i]);
+
+//                return array;
+                return java.util.Arrays.stream(array).map(i -> -i).toArray();
             }
+
         public static void main(String[] args) {
             int[] input = new int[] {-1,-2,-3,-4,-5};
         invert(input);
