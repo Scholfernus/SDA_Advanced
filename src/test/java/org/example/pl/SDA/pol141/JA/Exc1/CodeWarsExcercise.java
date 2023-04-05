0c2894e41ca04ab5a38f132d0147a2aaf878a182
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-05 19:48:36 +0200

    CodeWars - 4 days

--- src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExcercise.java
+++ src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExcercise.java
@@ -4,26 +4,52 @@ import java.util.Arrays;
 
 public class CodeWarsExcercise {
 
-            public static int[] invert ( int[] array){
+    public static int[] invert(int[] array) {
 
-               // int number = -1;
-                for (int i = 0; i < array.length; i++) {
-                    int invertn = array[i];
-                    array[i] = invertn * (-1);
-                }
-                for(int i = 0; i < array.length; i++) {
-                    array[i] *= -1;
-                }
-                for (int i = 0;i < array.length; i++)
-                    array[i] = -(array[i]);
+        // int number = -1;
+        for (int i = 0; i < array.length; i++) {
+            int invertn = array[i];
+            array[i] = invertn * (-1);
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= -1;
+        }
+        for (int i = 0; i < array.length; i++)
+            array[i] = -(array[i]);
 
 //                return array;
-                return java.util.Arrays.stream(array).map(i -> -i).toArray();
+        return java.util.Arrays.stream(array).map(i -> -i).toArray();
+    }
+
+    public static int points(String[] games) {
+        //implement me
+        int points = 0;
+        for (String g : games) {
+            String[] strings = g.split(":");
+            int x = Integer.parseInt(strings[0]);
+            int y = Integer.parseInt(strings[1]);
+            if (x > y) {
+                points += 3;
+            } else if (x == y) {
+                points++;
             }
 
-        public static void main(String[] args) {
-            int[] input = new int[] {-1,-2,-3,-4,-5};
+            System.out.print(Arrays.toString(strings));
+        }
+        return points;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] input = new int[]{-1, -2, -3, -4, -5};
         invert(input);
-            System.out.println(Arrays.toString(input));
+        System.out.println(Arrays.toString(input));
+        String[] total = {"1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"};
+        System.out.println(points(total));
+        System.out.println(total);
+        System.out.println("\n" + Arrays.toString(total));
+
     }
 }
