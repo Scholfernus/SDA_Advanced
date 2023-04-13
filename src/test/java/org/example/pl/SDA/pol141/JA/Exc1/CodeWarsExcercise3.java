e3cec02b660c76e0c1a4d0e09d54a8a1478e611d
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-13 21:33:50 +0200

    CodeWars - 12 days ćwiczenia -  end

--- src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExcercise3.java
+++ src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExcercise3.java
@@ -1,6 +1,7 @@
 package org.example.pl.SDA.pol141.JA.Exc1;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class CodeWarsExcercise3 {
     public static int findEvenIndex(int[] arr) {
@@ -16,10 +17,10 @@ public class CodeWarsExcercise3 {
             if (countLeft == countRight) {
                 index = j;
             }
-            countLeft -= arr[j-1];
-            }
-            return index;
-          //  int i, j; int leftsum, rightsum; int n = arr.length;
+            countLeft -= arr[j - 1];
+        }
+        return index;
+        //  int i, j; int leftsum, rightsum; int n = arr.length;
         //    for ( i = 0; i < n; ++i) {
         //      leftsum = 0; rightsum = 0;
         //      for ( j = 0; j < i; j++)
@@ -31,24 +32,34 @@ public class CodeWarsExcercise3 {
         //    }
         //    return -1;
         //  }
-        }
-    public static String toJadenCase(String phrase) {
-        StringBuilder fewWords = new StringBuilder();
-        String [] words = phrase.split( " ");
-        for ( String w : words ) {
-            String firstLetter = Character.toUpperCase(w.charAt(0));
-        }
-        return "";
     }
 
-   //  minMax number
+    public static String toJadenCase(String phrase) {
+        if (phrase.length() == 0 || phrase == null) {
+            return null;
+        }
+        StringBuilder fewWords = new StringBuilder();
+        String[] words = phrase.split(" ");
+        for (String w : words) {
+            if (w.length() <= 0) {
+                fewWords.append(" ");
+            }
+            char firstLetter = Character.toUpperCase(w.charAt(0));
+            String lastLetter = w.substring(1);
+            String letterConcat = firstLetter + lastLetter;
+            fewWords.append(letterConcat).append(" ");
+        }
+        return fewWords.toString().trim();
+    }
+
+    //  minMax number
     public static int[] minMax(int[] arr) {
         Arrays.sort(arr);
-        int[]finalNumber = new int[2];
+        int[] finalNumber = new int[2];
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             finalNumber[0] = arr[0];
-            finalNumber[1] = arr[arr.length-1];
+            finalNumber[1] = arr[arr.length - 1];
         }
         return finalNumber;
     }
@@ -57,14 +68,21 @@ public class CodeWarsExcercise3 {
         Arrays.sort(list);
         return list[0];
     }
+
     public int max(int[] list) {
         Arrays.sort(list);
-        return list[list.length-1];
+        return list[list.length - 1];
     }
+
 
     public static void main(String[] args) {
         System.out.println(findEvenIndex(new int[]{2824, 1774, -1490, -9084, -9696, 23094}));
-        System.out.println(Arrays.toString(minMax(new int[]{1,2,3,4,5})));
+        System.out.println(Arrays.toString(minMax(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(toJadenCase("turns out random test cases are easier than writing out basic ones"));
+        String[] arr={"1", "2", "3", "4", "5"};
+        Random r=new Random();
+        int randomNumber=r.nextInt(arr.length);
+        System.out.println(arr[randomNumber]);
     }
 }
 
