496cb55bf4ac8bd88d70c245c2043b0270237fad
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-15 22:25:01 +0200

    SDA ćwiczenie 03 - poprawiony kod

--- src/main/java/pl/sdajavapol141/Task03/Task03.java
+++ src/main/java/pl/sdajavapol141/Task03/Task03.java
@@ -13,17 +13,25 @@ public class Task03 {
         map.put("PHP", 0);
         map.put("C#",15);
         map.putIfAbsent("Java", 19);
-        for (var item : map.entrySet())
-        {
-            System.out.println("Wartość: " + item.getKey() + ", Wartość: " + item.getValue());
+        printMap(map);
+//        for (var item : map.entrySet())
+//        {
+//            System.out.println("Wartość: " + item.getKey() + ", Wartość: " + item.getValue());
+//        }
+//        extracted(map);
+//        map.entrySet().stream().map(item -> "Klucz " + item.getKey() + ", Wartość: " +
+//                + item.getValue()).collect(Collectors.joining("\n"));
+    }
+//
+//    private static void extracted(Map<String, Integer> map) {
+//        map.forEach((k, v)-> System.out.println("Klucz: " + k + ", Wartość " + v));
+//    }
+        private static void printMap(Map<String, Integer> map) {
+            String result = map
+                    .entrySet()
+                    .stream()
+                    .map(entry -> "Klucz: " + entry.getKey() + ", Wartość: " + entry.getValue())
+                    .collect(Collectors.joining(",\n")) + ".";
+            System.out.println(result);
         }
-        extracted(map);
-        map.entrySet().stream().map(item -> "Klucz " + item.getKey() + ", Wartość: " +
-                + item.getValue()).collect(Collectors.joining("\n"));
-    }
-
-    private static void extracted(Map<String, Integer> map) {
-        map.forEach((k, v)-> System.out.println("Klucz: " + k + ", Wartość " + v));
-    }
 }
-// trzeba dokończyć ten kod z Git-a
