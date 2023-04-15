7cb23e2302c7d5337f22f55bbd645ac83b114125
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-15 16:22:48 +0200

    SDA ćwiczenia od 01 do 08 - day 1

--- src/main/java/pl/sdajavapol141/Powtorka/MethodApp.java
+++ src/main/java/pl/sdajavapol141/Powtorka/MethodApp.java
@@ -3,5 +3,15 @@ package pl.sdajavapol141.Powtorka;
 public class MethodApp {
     public static void main(String[] args) {
 
+        String b1 = bulka(0.1);
+        String b2 = bulka(0.5);
+        System.out.println(b2);
+        System.out.println(b1);
+    }
+    public static String bulka(double maka){
+        System.out.println("Weź mąkę");
+        System.out.println("Wymieszaj mąkę z wodą");
+        int ileBulek = (int)(maka/0.1);
+        return "Bułka ".repeat(ileBulek);
     }
 }
