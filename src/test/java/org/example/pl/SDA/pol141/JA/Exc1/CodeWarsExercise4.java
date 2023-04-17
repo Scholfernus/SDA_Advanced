6e47969b4531ac3ccd7a86354f532f2fbd130d92
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-17 18:31:24 +0200

    CodeWars - 13 days ćwiczenia - end

--- src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExercise4.java
+++ src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExercise4.java
@@ -1,11 +1,14 @@
 package org.example.pl.SDA.pol141.JA.Exc1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CodeWarsExercise4 {
     public static void main(String[] args) {
         System.out.println(high("what time are we climbing up to the volcano"));
         System.out.println(longestWord("what time are we climbing up to the volcano"));
+        System.out.println(whoLikesIt("Alex", "Jacob", "Mark", "Max"));
     }
 
     // Highest Scoring Word
@@ -49,5 +52,42 @@ public class CodeWarsExercise4 {
     public static int square(int n) {
         return n * n;
         //Your Code
+    }
+
+    // Who likes it?
+    public static String whoLikesIt(String... names) {
+        List<String> nameAll = new ArrayList<>(Arrays.asList(names));
+        int namesCount = nameAll.size();
+        switch (namesCount) {
+            case 0:
+                return "no one likes this";
+            case 1:
+                return String.format("%s and %s likes this", nameAll.get(0));
+            case 2:
+                return String.format("%s and %s like this", nameAll.get(0), nameAll.get(1));
+            case 3:
+                return String.format("%s, %s and %s like this", nameAll.get(0), nameAll.get(1), nameAll.get(2));
+            default:
+                return String.format("%s, %s and %d others like this", nameAll.get(0), nameAll.get(1),namesCount-2);
+        }
+        //    switch (names.length) {
+        //          case 0: return "no one likes this";
+        //          case 1: return String.format("%s likes this", names[0]);
+        //          case 2: return String.format("%s and %s like this", names[0], names[1]);
+        //          case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+        //          default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        //        }
+    }
+    public static double sum(double[] numbers) {
+        double sum = 0;
+        for (var d : numbers) {
+            if (numbers.length == 0){
+                return 0;
+            }else {
+                sum +=d;
+            }
+        }
+        return sum;
+        // return Arrays.stream(numbers).sum();
     }
 }
