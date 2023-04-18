383e2ed2a99e068b11fca9213a0f856aa18bbb80
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-18 21:51:05 +0200

    CodeWars - 15 days ćwiczenia - end

--- src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExercise4.java
+++ src/test/java/org/example/pl/SDA/pol141/JA/Exc1/CodeWarsExercise4.java
@@ -213,5 +213,28 @@ public class CodeWarsExercise4 {
         }
         return result;
     }
+
+    public static String howMuchILoveYou(int nb_petals) {
+        int petals = nb_petals % 6;
+        switch (petals) {
+            case 1:
+                return "I love you";
+            case 2:
+                return "a little";
+            case 3:
+                return "a lot";
+            case 4:
+                return "passionately";
+            case 5:
+                return "madly";
+            default:
+                return "not at all";
+        }
+        // String[] arr ={"not at all", "I love you",  "a little", "a lot", "passionately", "madly"};
+        //      return arr[nb_petals%6];
+    }
 }
+
+
+
 
