9da4ac392819d1553d03882f64e532af63073a19
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-04-26 22:32:55 +0200

    SDA ćwiczenia Java zaawansowana Book

---
+++ src/test/java/org/example/ZadaniaProgramAdvancedPDF/Exc2_BooK/NoBookFoundException.java
@@ -0,0 +1,7 @@
+package org.example.ZadaniaProgramAdvancedPDF.Exc2_BooK;
+
+public class NoBookFoundException extends Exception{
+    public NoBookFoundException(String message){
+        super(message);
+    }
+}
